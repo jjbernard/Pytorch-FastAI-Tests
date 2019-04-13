909b232def6be0ea005e006c0ed894e16fa4009e
Jean-Jacques Bernard <jjb@cantab.net>
2019-04-13 20:06:57 +0200

    Initial commit, code not working yet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pytorch-FastAI-Tests
+
+In this repository I store some tests with both the PyTorch and FastAI libraries.
---
+++ lstm01.py
@@ -0,0 +1,126 @@
+# Imports
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+# We are going to implement Jason Brownlee basic LSTM code using PyTorch instead of Keras.
+# See https://machinelearningmastery.com/how-to-develop-lstm-models-for-time-series-forecasting/ for more
+# details. The data preparation part comes directly from his example
+
+# Parameters
+# n_pred (number of steps we want to predict) should be equal to output_size ?
+n_steps = 5
+n_pred = 1
+bs = 32
+max_epochs = 60
+hidden_size = 5
+output_size = 1
+input_size = 1
+num_layers = 10
+lr = 0.003
+
+# Should hidden size = the size of the sequence ???
+
+# Data preparation 
+
+def split_sequence(sequence, n_steps, n_pred):
+    #X, y = [], []
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        # look for the end of this pattern
+        end_idx = i + n_steps
+        # check if we are beyon the end of the sequence
+        if end_idx > len(sequence) - n_pred - 1:
+            break
+        # Prepare inputs and outputs of the sequence
+        seq_X, seq_y = sequence[i:end_idx], sequence[end_idx+1:end_idx+n_pred+1]
+        X.append(seq_X)
+        y.append(seq_y)
+    return torch.tensor(X).float(), torch.tensor(y).float()
+
+seq = list(range(1000))
+
+X, y = split_sequence(seq, n_steps, n_pred)
+
+# Let's now define our dataset and dataloader using PyTorch tools
+
+idx = int(len(X) * 0.8)
+train = TensorDataset(X[:idx], y[:idx])
+valid = TensorDataset(X[idx:], y[idx:])
+
+train_loader = DataLoader(train, batch_size=bs, shuffle=False)
+valid_loader = DataLoader(valid, batch_size=2, shuffle=False)
+
+# Let's now define our LSTM Model
+
+class MyLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(MyLSTM,self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        # input_size is equal to the number of features
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        # Output of LSTM layers will be [batch_size, seq_length, input_size]
+        self.linear = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input):
+        # Initialize initial state for h0 and c0
+        # input.size(0) actually corresponds to the batch size here
+        h0 = torch.zeros(self.num_layers, input.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, input.size(0), self.hidden_size)
+
+        # print('h0 shape: ', h0.shape)
+        # print('c0 shape: ', c0.shape)
+        # print('input shape: ', input.shape)
+
+        # Forward propagate the input into the LSTM
+        result, _ = self.lstm(input, (c0, h0))
+        result = result[:,-1,:]
+        # print('result shape: ', result.shape)
+        result = self.linear(result)
+        return result
+
+model = MyLSTM(input_size, hidden_size, num_layers, output_size)
+
+criterion = nn.MSELoss()
+optim = optim.Adam(model.parameters(), lr=lr)
+
+total_steps = len(train_loader)
+
+lossdata = []
+for epoch in range(max_epochs):
+    for i, (X, y) in enumerate(train_loader):
+        X = torch.reshape(X,(X.size(0), X.size(1), input_size))
+        out = model(X)
+        # print(out)
+        # print('out shape: ', out.shape)
+        # print('y shape: ', y.shape)
+        loss = criterion(out, y)
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+        if i % 100 == 0:
+            lossdata.append([epoch+1,loss.item()])
+            print('Epoch = {}/{}, Loss: {:.4f}'.format(epoch+1, max_epochs, loss.item()))
+            # print('y = ', y)
+            # print('out = ', out)
+
+plt.plot(lossdata)
+plt.show()
+
+with torch.no_grad():
+    for i, (X, y) in enumerate(valid_loader):
+        X = torch.reshape(X, (X.size(0), X.size(1), input_size))
+        out = model(X)
+        if i % 50 == 0:
+            print('X = ', X)
+            print('Prediction = ', out)
+            print('Ground Truth = ', y)
+
+# Does not work so far, is this because of the initialization of h0 and c0 ???
