04d26b7a565fe28926e2c11f887406dda654d230
Jean-Jacques Bernard <jjb@cantab.net>
2019-04-13 22:13:53 +0200

    10x datapoints

--- lstm01.py
+++ lstm01.py
@@ -43,7 +43,7 @@ def split_sequence(sequence, n_steps, n_pred):
         y.append(seq_y)
     return torch.tensor(X).float(), torch.tensor(y).float()
 
-seq = list(range(1000))
+seq = list(range(10000))
 
 X, y = split_sequence(seq, n_steps, n_pred)
 
